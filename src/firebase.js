9294a93f43e7a8e3ad18ef817566384d8482dc05
Daniel Plata <danielplata79@gmail.com>
2023-10-04 17:51:33 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCacBFectYHKrMzD7mulTkT1oCLqRM19lw",
+  authDomain: "whastsapp-clone-56b27.firebaseapp.com",
+  projectId: "whastsapp-clone-56b27",
+  storageBucket: "whastsapp-clone-56b27.appspot.com",
+  messagingSenderId: "532666891462",
+  appId: "1:532666891462:web:578d8cff0578188b4d6bc7",
+  measurementId: "G-TYMNYEBBPS"
+};
